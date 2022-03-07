b30d26e468136613e0a34f66cfa67fc953e295e3
AlfonsoAHR <56330213+AlfonsoAHR@users.noreply.github.com>
2022-03-07 00:17:40 -0600

    Update asistentes.php

--- asistentes.php
+++ asistentes.php
@@ -16,18 +16,22 @@ $conexion = mysqli_connect($servidor, $usuario, $contrasena, $bd);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleAsistentes.css">
     <title>Tabla de usuarios</title>
 </head>
 
 <body>
-    <h1>Tabla de Usuarioss</h1>
+    <h1>Tabla de Usuarios</h1>
     <br>
-    <table border="1">
-        <tr>
-            <td>id</td>
-            <td>usuario</td>
-            <td>rol</td>
-        </tr>
+    <div id="main-container">
+    <table>
+        <thead>
+            <tr>
+            <th>id</th>
+            <th>usuario</th>
+            <th>rol</th>
+            </tr>
+        </thead>
         <?php
         $sql = "select id, usuario,rol from usuarios";
         $result = mysqli_query($conexion, $sql);
@@ -35,13 +39,18 @@ $conexion = mysqli_connect($servidor, $usuario, $contrasena, $bd);
         ?>
         <tr>
             <td><?php echo $mostrar['id'] ?></td>
+        </tr>
+        <tr>    
             <td><?php echo $mostrar['usuario'] ?></td>
+        </tr>
+        <tr>
             <td><?php echo $mostrar['rol'] ?></td>
         </tr>
         <?php
         }
         ?>
     </table>
+    </div>
     <br />
     <button onclick="location.href='index.php'">REGRESAR</button>
 </body>
