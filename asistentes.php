c592de92a8d7aaa85e8f804838a55f70b75388f6
AlfonsoAHR <56330213+AlfonsoAHR@users.noreply.github.com>
2022-03-07 01:03:02 -0600

    Update asistentes.php

--- asistentes.php
+++ asistentes.php
@@ -17,19 +17,25 @@ $conexion = mysqli_connect($servidor, $usuario, $contrasena, $bd);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styleAsistentes.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet"> 
     <title>Tabla de usuarios</title>
 </head>
 
 <body>
-    <h1>Tabla de Usuarios</h1>
+     <div class="bg-sec">
+
+    </div>
+    <h1>USERS</h1>
     <br>
     <div id="main-container">
     <table>
         <thead>
             <tr>
-            <th>id</th>
-            <th>usuario</th>
-            <th>rol</th>
+            <th>ID</th>
+            <th>USER</th>
+            <th>ROL</th>
             </tr>
         </thead>
         <?php
@@ -52,7 +58,11 @@ $conexion = mysqli_connect($servidor, $usuario, $contrasena, $bd);
     </table>
     </div>
     <br />
-    <button onclick="location.href='index.php'">REGRESAR</button>
+    <button class="btn btn--2" onclick="location.href='index.html'" >EXIT</button>
+
+     <div class="bg-sec">
+
+    </div>
 </body>
 
 </html>
